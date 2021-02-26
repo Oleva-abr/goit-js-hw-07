b73e187182edf5d944dd9d4d0b2a464653c51810
Olena Abramova <olena.v.abramova@gmail.com>
2021-02-26 09:14:28 +0200

    add 4

--- js/task-4.js
+++ js/task-4.js
@@ -4,4 +4,26 @@
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
-const counterValue = 0;
+const button = {
+  inc: document.querySelector("[data-action='increment']"),
+  decr: document.querySelector("[data-action='decrement']"),
+  span: document.querySelector("#value"),
+};
+
+let counterValue = 0;
+
+const increment = () => {
+  counterValue += 1;
+
+ button.span.textContent = counterValue;
+};
+
+const decrement = () => {
+  counterValue -= 1;
+
+  button.span.textContent = counterValue;
+};
+
+button.inc.addEventListener("click", increment);
+button.decr.addEventListener("click", decrement);
+
